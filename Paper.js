ba064dae16fa1f96d6ef285a299ac13d4072dbde
Josef Kuchař <email@josefkuchar.cz>
2017-03-25 22:49:30 +0100

    First version

---
+++ Paper.js
@@ -0,0 +1,107 @@
+function Paper(size) {
+    this.size = size;
+    this.board = this.createBoard(size);
+    this.currentPlayer = 0;
+    console.log(this.board);
+}
+
+Paper.prototype.createBoard = function(size) {
+    var array = [];
+    for (var i = 0; i < size; i++) {
+        array[i] = [];
+        for (var j = 0; j < size; j++) {
+            array[i][j] = 0;
+        }
+    }
+    return array;
+}
+
+Paper.prototype.render = function() {
+    this.renderBoard();
+    this.renderFields();
+}
+
+Paper.prototype.renderBoard = function() {
+    var area = world.width - offset * 2;
+    var step = area / this.size;
+
+    strokeWeight(5 * SCALE);
+    stroke(202);
+    for (var i = 1; i < this.size; i++) {
+        line((offset + i * step) * SCALE, offset * SCALE, (offset + i * step) * SCALE, (world.height - offset) * SCALE);
+    }
+
+    for (var i = 1; i < this.size; i++) {
+        line(offset * SCALE, (offset + i * step) * SCALE, (world.width - offset) * SCALE, (offset + i * step) * SCALE);
+    } 
+}
+
+Paper.prototype.renderFields = function() {
+    var area = world.width - offset * 2;
+    var step = area / this.size;
+
+    for (var x = 0; x < this.size; x++) {
+        for (var y = 0; y < this.size; y++) {
+            noFill();
+            if (this.board[x][y] == 1) {
+                stroke(52, 152, 219);
+                line(((offset + x * step + step / 2) - step / 5) * SCALE, ((offset + y * step + step / 2) - step / 5) * SCALE, ((offset + x * step + step / 2) + step / 5) * SCALE, ((offset + y * step + step / 2) + step / 5) * SCALE)
+                line(((offset + x * step + step / 2) - step / 5) * SCALE, ((offset + y * step + step / 2) + step / 5) * SCALE, ((offset + x * step + step / 2) + step / 5) * SCALE, ((offset + y * step + step / 2) - step / 5) * SCALE)
+            }
+            else if (this.board[x][y] == 2) {
+                stroke(231, 76, 60);
+                ellipse((offset + x * step + step / 2) * SCALE, (offset + y * step + step / 2) * SCALE, (step / 2) * SCALE)
+            }
+        }
+    }
+}
+
+Paper.prototype.setField = function(x, y) {
+    if (this.board[x][y] == 0) {
+        this.board[x][y] = this.currentPlayer + 1;
+        this.currentPlayer++;
+        this.currentPlayer %= 2;
+    }
+}
+
+Paper.prototype.gameDone = function() {
+    for (var x = 0; x < this.size; x++) {
+        for (var y = 0; y < this.size; y++) {
+            if (this.board[x][y] === 0) {
+                continue
+            }
+
+            // Collumn
+            if (this.safeSpot(x, y - 4) && this.equal(x, y, x, y + 1, x, y + 2, x, y + 3, x, y + 4)) {
+                return true
+            }
+            // Row
+            else if (this.safeSpot(x + 4, y) && this.equal(x, y, x + 1, y, x + 2, y, x + 3, y, x + 4, y)) {
+                return true
+            }
+            // Diagonal right
+            else if (this.safeSpot(x + 4, y) && this.equal(x, y, x + 1, y - 1, x + 2, y - 2, x + 3, y - 3, x + 4, y - 4)) {
+                return true
+            }
+            // Diagonal left
+            else if (this.safeSpot(x + 4, y) && this.equal(x, y, x + 1, y + 1, x + 2, y + 2, x + 3, y + 3, x + 4, y + 4)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+Paper.prototype.safeSpot = function(x, y) {
+    return x > 0 && x < this.size && y > 0 && y < this.size
+}
+
+Paper.prototype.equal = function(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5) {
+    return this.board[x1][y1] === this.board[x2][y2] && this.board[x2][y2] === this.board[x3][y3] &&
+        this.board[x3][y3] === this.board[x4][y4] && this.board[x4][y4] === this.board[x5][y5]
+}
+
+Paper.prototype.reset = function() {
+    this.board = this.createBoard(this.size);
+    this.currentPlayer = 0;
+}
---
+++ sketch.js
@@ -0,0 +1,96 @@
+var ratioW = 1;
+var ratioH = 1;
+var RATIO = ratioW / ratioH;
+var world = {
+    width: ratioW * 1000,
+    height: ratioH * 1000
+}
+var SCALE = 0;
+var offset = 70;
+var end = false;
+
+var paper;
+
+function setup() {
+    if (windowHeight < windowWidth) {
+        createCanvas(windowHeight * RATIO, windowHeight);
+    } else {
+        createCanvas(windowWidth, windowWidth * RATIO);
+    }
+
+    paper = new Paper(15);
+}
+
+function draw() {
+    background(238);
+
+    updateScale();
+
+    paper.render();
+
+    renderText();
+
+    if(paper.gameDone()) {
+        fill(255);
+        rect(width / 4, height / 4, width / 2, height / 2);
+        
+        var current = paper.currentPlayer == 0 ? "O" : "X";
+        fill(51);
+        textSize(60 * SCALE);
+        text("Player " + current + " wins!", width / 2, height / 2);
+        textSize(25 * SCALE);
+        text("Press any key to restart game", width / 2, height / 2 + 50 * SCALE);
+        end = true;
+    }
+}
+
+function windowResized() {
+    if (windowHeight < windowWidth) {
+        resizeCanvas(windowHeight * RATIO, windowHeight);
+    } else {
+        resizeCanvas(windowWidth, windowWidth * RATIO);
+    }
+}
+
+function updateScale() {
+    SCALE = width / world.width;
+}
+
+function mousePressed() {
+    if (end) {
+        paper.reset();
+        end = false
+        return false
+    }
+
+    var area = world.width - offset * 2;
+    var step = area / paper.size;
+
+    var gameX = mouseX / SCALE;
+    var gameY = mouseY / SCALE;
+    
+    var boardX = Math.floor((gameX - offset) / step);
+    var boardY = Math.floor((gameY - offset) / step);
+
+    if (boardX >= 0 && boardX < paper.size && boardY >= 0 && boardY < paper.size) {
+        paper.setField(boardX, boardY);
+    }
+}
+
+function keyPressed() {
+    if (end) {
+        paper.reset();
+        end = false
+    }
+}
+
+function renderText() {
+    noStroke();
+    fill(51);
+    textAlign(CENTER);
+    textSize(50 * SCALE);
+
+    var current = paper.currentPlayer == 0 ? "X" : "O";
+
+    text(current + " on turn", width / 2, (offset - 20) * SCALE);
+}
